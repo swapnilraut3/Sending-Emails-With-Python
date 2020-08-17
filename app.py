713a7b6efdcf12f3f98d1acf693da01eea316c9b
swapnilraut3 <swapnil.purandar@gmail.com>
2020-08-17 20:13:22 +0530

    feat: import dotenv & load env variables - add both body & html in content

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
 import os
 import yagmail
+from pipenv.vendor.dotenv import load_dotenv, find_dotenv
 
 if __name__ == "__main__":
+    load_dotenv()
+
     recipients_list = ['recipient1@gmail.com', 'recipient2@gmail.com']
     subject = 'Hello, mail from Python email'
-    contents = 'Body of "Hello, mail from Python"'
+    body = 'Body of "Hello, mail from Python"'
+    html = '''
+    <html>
+        <body>
+             <p>Hi,<br>
+                How are you? Doing Good?<br>
+                <a href="http://www.realpython.com">Real Python</a> 
+                has many great tutorials.
+                </p>
+        </body>
+    </html>
+    '''
     attachments_list = ['attachment.pdf', 'attachment.jpeg']
 
     user_name = os.environ.get('GMAIL_USR', None)
@@ -21,7 +35,7 @@ if __name__ == "__main__":
     mygmail = yagmail.SMTP(user=user_name, password=password)
 
     try:
-        mygmail.send(to=recipients, subject=subject,
-                     contents=contents, attachments=attachments)
+        mygmail.send(to=recipients_list, subject=subject,
+                     contents=[body, html], attachments=attachments_list)
     except Exception as e:
         print(e)
